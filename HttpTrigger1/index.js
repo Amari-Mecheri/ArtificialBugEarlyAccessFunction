1a89aeab3875e791ade3c0b10c67aa0a69bc5efb
Amari-Mecheri <amari.mecheri@gmail.com>
2025-11-10 16:18:07 +0100

    headers

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -21,13 +21,13 @@ const config = {
 module.exports = async function (context, req) {
     context.log('Function started');
     
-    // // CORS headers
-    // const headers = {
-    //     'Access-Control-Allow-Origin': '*',
-    //     'Access-Control-Allow-Methods': 'POST, OPTIONS',
-    //     'Access-Control-Allow-Headers': 'Content-Type',
-    //     'Content-Type': 'application/json'
-    // };
+    // CORS headers
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Content-Type': 'application/json'
+    };
 
     // Handle OPTIONS
     if (req.method === 'OPTIONS') {
